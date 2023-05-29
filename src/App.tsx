58da38d17674919b3b9fdd9e85a74d258943a59c
Marcus Lourenço <vini10310@gmail.com>
2023-05-29 20:27:04 -0300

    feat: Added tests

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,32 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const [auth, setToken] =useState<string>('');
+  const [token, setToken] =useState<string>('');
 
-  let headersList = {
-    Accept: "*/*",
-    "Content-Type": "application/json",
-  };
+  const [EI, setEI] =useState<string>('');
 
-  let bodyContent = JSON.stringify({
-    inepCod: "26127792",
-    password: "26127792",
-  });
-
-  let reqOptions = {
-    url: "https://motionless-red-donkey.cyclic.app/auth/login",
-    method: "POST",
-    headers: headersList,
-    data: bodyContent,
-  };
+  const [data, setData] =useState<string>('');
 
   const fetchToken = async () => {
     try {
+
+      let headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      };
+    
+      let bodyContent = JSON.stringify({
+        inepCod: "26127792",
+        password: "26127792",
+      });
+    
+      let reqOptions = {
+        url: "https://motionless-red-donkey.cyclic.app/auth/login",
+        method: "POST",
+        headers: headersList,
+        data: bodyContent,
+      };
+
       const response = await axios.request(reqOptions); // Replace with your API endpoint
       console.log(response)
       setToken(response.data.access_token);
@@ -35,7 +40,66 @@ function App() {
     }
   };
 
+  const fetchEI = async () => {
+    try {
+      let headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      };
+    
+      let reqOptions = {
+        url: "https://motionless-red-donkey.cyclic.app/educational-institution",
+        method: "GET",
+        headers: headersList,
+      };
 
+      const response = await axios.request(reqOptions); // Replace with your API endpoint
+      console.log(response)
+      const data = await JSON.stringify(response.data)
+      setEI(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+  const postData = async () => {
+    try {
+      let headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": token
+      };
+    
+
+      let bodyContent = JSON.stringify({
+        "activity": "Horta",
+        "qtyStudent": 4,
+        "qtyEducator": 1,
+        "infoStudent":
+          "Estudantes divididos em uma equipe, equipe é responsável por um tipo de hortaliça tipo de hortaliça",
+        "materials":
+          "Composteiras de três compartimentos : 2 ; Composteiras (garrafa pet), qty: 3",
+        "inepCod": "26127792"
+      });
+
+
+      let reqOptions = {
+        url: "https://motionless-red-donkey.cyclic.app/internal-use",
+        method: "POST",
+        headers: headersList,
+        data: bodyContent
+      };
+
+      const response = await axios.request(reqOptions); // Replace with your API endpoint
+      console.log(response)
+      const data = await JSON.stringify(response.data)
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
     <div className="App">
@@ -57,8 +121,16 @@ function App() {
         </p>
       </div>
       <div>
-      <button onClick={fetchToken}>Fetch Data</button>
-        <div>{auth}</div>
+      <button onClick={fetchToken}>Fetch Token</button>
+        <div>{token}</div>
+      </div>
+      <div>
+      <button onClick={fetchEI}>Fetch Data</button>
+        <div>{EI}</div>
+      </div>
+      <div>
+      <button onClick={fetchEI}>Fetch Data</button>
+        <div>{data}</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
